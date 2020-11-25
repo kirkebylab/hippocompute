896dde41201e658830f031d2aed40282d2220aed
tstannius <tobiasostannius@gmail.com>
2020-11-25 18:20:19 +0100

    Add H9-RC17 combined dataset

--- R/helpers_qrt_pcr.R
+++ R/helpers_qrt_pcr.R
@@ -15,7 +15,9 @@ load_all_reference_data <- function() {
   dsets[[ "h9_v0_2018" ]] <- read.csv("data/h9_v0_2018.tsv", header=TRUE, sep='\t', stringsAsFactors=FALSE)
   dsets[[ "h9_v0_2020" ]] <- read.csv("data/h9_v0_2020.tsv", header=TRUE, sep='\t', stringsAsFactors=FALSE)
   dsets[[ "h9_v1_2020" ]] <- read.csv("data/h9_v1_2020.tsv", header=TRUE, sep='\t', stringsAsFactors=FALSE)
+  # dsets[[ "h9-rc17_v1_2020" ]] <- read.csv("data/h9-rc17_v1_2020.tsv", header=TRUE, sep='\t', stringsAsFactors=FALSE)
   dsets[[ "rc17_v1_2020" ]] <- read.csv("data/rc17_v1_2020.tsv", header=TRUE, sep='\t', stringsAsFactors=FALSE)
+  dsets[[ "h9-rc17_v1_2020" ]] <- rbind(dsets[[ "h9_v1_2020" ]], dsets[[ "rc17_v1_2020" ]])
   return(dsets)
 }
 
