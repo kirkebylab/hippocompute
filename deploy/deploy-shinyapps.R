ed7914a3fe9a312af1bd394aaeff4bcab2e6ee11
tstannius <tobiasostannius@gmail.com>
2021-07-17 19:42:16 +0200

    Try fix rsconnect dependency

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -1,9 +1,10 @@
+suppressMessages(library(rsconnect))
 # deploy/deploy-shinyapps.R
 # usethis::use_build_ignore("deploy")
 rsconnect::setAccountInfo(
-  Sys.getenv("SHINYAPPS_ACCOUNT"),
-  Sys.getenv("SHINYAPPS_TOKEN"),
-  Sys.getenv("SHINYAPPS_SECRET")
+  Sys.getenv("SHINYAPPS_ACCOUNT"), # repo secret in GitHub settings
+  Sys.getenv("SHINYAPPS_TOKEN"),   # repo secret in GitHub settings
+  Sys.getenv("SHINYAPPS_SECRET")   # repo secret in GitHub settings
 )
 rsconnect::deployApp(
   appName = "hippocompute",
